ec9d0ca93ba2e0cec80354d9bf5530ae5283b57f
Yongcheng Ji <gallen83@163.com>
2018-01-03 15:30:24 +1100

    Update substitute-env-vars.sh

--- config/installer/bin/substitute-env-vars.sh
+++ config/installer/bin/substitute-env-vars.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+#-e Exit immediately if a command exits with a non-zero exit status.
+#-u Treat unset variables as an error when substituting.
 set -eu
 
+# $# is the number of arguments
 output_dir=$1
+# about shift http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_07.html
 shift
+# set the files-to-search to the args passed.
 files=$@
 
 function fill_in() {
